1b4bf947712676ce068ca659039403fc74909d94
Cindy Conway <cconway@upwardsports.org>
2026-06-25 15:50:50 -0400

    adding github workflows

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,3 +1,3 @@
 import createClient from "openapi-fetch";
 import type { paths } from "@/api/schema";
-export const api = createClient<paths>({ baseUrl: import.meta.env.VITE_API_URL });
+export const api = createClient<paths>({ baseUrl: import.meta.env.VITE_API_URL });cd
